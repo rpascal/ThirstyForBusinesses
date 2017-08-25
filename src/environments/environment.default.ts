b7bcb198b7002a4587580c187e3ba7ca3e486ccf
rpascal <ryanpascal99@yahoo.com>
2017-08-24 22:16:21 -0400

    Init

---
+++ src/environments/environment.default.ts
@@ -0,0 +1,22 @@
+export const ENVIRONMENT = {
+  environment : "default",
+  firebaseConfig: {
+    apiKey: "AIzaSyAHMv3o3sj62eOnINMcGGMVpSAoq85hW4c",
+    authDomain: "social-5ad5b.firebaseapp.com",
+    databaseURL: "https://social-5ad5b.firebaseio.com",
+    projectId: "social-5ad5b",
+    storageBucket: "social-5ad5b.appspot.com",
+    messagingSenderId: "153122904996"
+  },
+  firebaseDatabasePaths : {
+    users : "_users",
+    // businesses : "_businesses",
+    businesses : "_businessDummt",
+    
+    promos : "_promos"
+  },
+  facebookAppId: "1025234637591184",
+  googleClientId: "31493597450-u75kd39sk6f8q6r4bfh807oush6tq7lu.apps.googleusercontent.com",
+  googleMapsAPIKey: "AIzaSyAxKKrdC08TkrbHw5SNmQzhW6TareXXFwI"
+
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ENVIRONMENT} from "../../environments/environment.default"
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,8 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    console.log(ENVIRONMENT.environment)
+    
   }
 
 }
